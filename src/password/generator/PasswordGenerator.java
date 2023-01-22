9a87c2ec102f3dec35eb7312f27dd95def93bb84
Nielark <markdanielmagpantay2001@gmail.com>
2023-01-22 18:12:28 +0800

    First commit

---
+++ src/password/generator/PasswordGenerator.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package password.generator;
+
+/**
+ *
+ * @author Mark Daniel
+ */
+public class PasswordGenerator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new PasswordGeneratorUI().setVisible(true);
+    }
+    
+}
